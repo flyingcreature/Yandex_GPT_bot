8f261ed2f5385cd31a4ccf8a3399d17ae2dd4d07
Кирилл <kirillkazakov705@gmail.com>
2024-05-02 10:00:39 +0300

    добавлен модуль получения токена

--- utils.py
+++ utils.py
@@ -1,13 +1,8 @@
 import json
 import logging
 import math
-import time
-
-import requests
 
 from config import (
-    IAM_TOKEN_ENDPOINT,
-    IAM_TOKEN_PATH,
     LOGS_PATH,
     MAX_USER_GPT_TOKENS,
     MAX_USER_STT_BLOCKS,
@@ -47,60 +42,6 @@ def save_data(data: dict, path: str) -> None:
     """
     with open(path, "w", encoding="utf-8") as file:
         json.dump(data, file, ensure_ascii=False, indent=2)
-
-
-def create_new_iam_token():
-    """
-    Получает новый IAM-TOKEN и дату истечения его срока годности и
-    записывает полученные данные в json
-    """
-    headers = {"Metadata-Flavor": "Google"}
-
-    try:
-        response = requests.get(IAM_TOKEN_ENDPOINT, headers=headers)
-
-    except Exception as e:
-        print(f"Не удалось выполнить запрос: {e}, токен не получен")
-        logging.error(f"Не удалось выполнить запрос: {e}, токен не получен")
-    else:
-        if response.status_code == 200:
-            token_data = {
-                "access_token": response.json().get("access_token"),
-                "expires_at": response.json().get("expires_in") + time.time(),
-            }
-
-            with open(IAM_TOKEN_PATH, "w") as token_file:
-                json.dump(token_data, token_file)
-                logging.info("Iam токен создан")
-        else:
-            print(
-                f"Ошибка при получении ответа: {response.status_code}, токен не получен"
-            )
-            logging.error(
-                f"Ошибка при получении ответа: {response.status_code}, токен не получен"
-            )
-
-
-def get_iam_token() -> str:
-    """
-    Получает действующий IAM-TOKEN и возвращает его
-    """
-    try:
-        with open(IAM_TOKEN_PATH, "r") as token_file:
-            token_data = json.load(token_file)
-
-        expires_at = token_data.get("expires_at")
-
-        if expires_at <= time.time():
-            create_new_iam_token()
-
-    except FileNotFoundError:
-        create_new_iam_token()
-
-    with open(IAM_TOKEN_PATH, "r") as token_file:
-        token_data = json.load(token_file)
-
-    return token_data.get("access_token")
 
 
 def check_number_of_users(user_id: int) -> tuple[bool | None, str]:
